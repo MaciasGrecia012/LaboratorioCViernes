1e9fe8d163ea13bf2abc69c7d7810bfe6bc3dad4
MaciasGrecia012 <grecia_secrets@hotmail.com>
2019-10-04 16:57:26 -0500

    Practica9

--- main.c
+++ main.c
@@ -2,17 +2,42 @@
 //Practica 9
 //Actividad 1
 #include <stdio.h>
+#include <time.h>
 int main(){
-int ventanasdeEmpleadosOficinaEdificios[6][10][2];
-int i, j, k, a;
-for(i=0; i<6; i++){
-	for(j=0; j<10; j++){
-		for(k=0; j<2, j++);
-		printf("\n\tIngrese un numero:")
-		scanf("%d", &ventanasEmpleadosOficinaEdificios[i][j][k])
-	 if(i==3 && j==7 && k==2){
-	 	ventanasEmpleadosOficinaEdificios[i][j][k]=4234;
-	 	printf("el empleado numero %d esta en la ")
+	 //Inicializacion del arreglo tridimencional de tipo entero
+int VEOE[6][10][2];
+//inicializacion de las variables tipo enteras
+int i, j, k, a; 
+//ciclo para el llenado y la impresion  de la matriz
+for(i=0; i<6; i++){ 
+//ciclo para el llenado y la impresion de la matriz
+	for(j=0; j<10; j++){  
+	//ciclo para el llenado y la impresion de la matriz	
+		for(k=0; k<2; k++){  
+		//Llenado de la matriz de manera random
+		 VEOE[i][j][k]=rand(); 
+		 //llenado en una posicion especifica de la matriz
+	 	VEOE[3][7][2]=4234; 
+	 	//impresion de la matriz en cada una de sus pociciones junto a su valor
+		printf("\n\tEl empleado numero %d de la oficina %d del edificio numero %d esta ha vendido %d unidades", i, j, k, VEOE[i][j][k]); 
+	}
+}
+}
+//Espacios para separar la actividad 1 de la 2
+printf("\n\n\n\n\n\n"); 
+//actividad 2
+//inicializacion de una nueva matriz en la que se le va a guardar los valores de la anterior incrementados
+int nuev[6][10][2];  
+//ciclo para el llenado y la impresion  de la matriz
+for(i=0; i<6; i++){ 
+//ciclo para el llenado y la impresion  de la matriz
+	for(j=0; j<10; j++){ 
+	//ciclo para el llenado y la impresion  de la matriz
+		for(k=0; k<2; k++){
+		 //Llenado de la nueva matriz con la matreiz anteriormente llenada de manera random sumandole  10
+	 nuev[i][j][k]=VEOE[i][j][k]+10;
+	 	//impresion de la matriz en cada una de sus pociciones y con su valor ya incrementado
+  printf("\n\tEl empleado numero %d de la oficina %d del edificio numero %d esta ha vendido %d unidades", i, j, k, VEOE[i][j][k]);    
 		}
 	}
 }
